7c8c38220494b24de9db4303c7855e227889ce93
Nguyen Hai Dang <nguyenhaidang.uit@gmail.com>
2016-01-04 05:17:35 +0700

    load thong tin khen thuong, remove unused code

---
+++ SMS/SMS.SERVICE/IService/IKhenThuongService.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using SMS.CORE.Data;
+
+namespace SMS.SERVICE.IService
+{
+    /// <summary>
+    /// IService khenthuong
+    /// </summary>
+    public interface IKhenThuongService
+    {
+        /// <summary>
+        /// get all khenthuong
+        /// </summary>
+        /// <returns>list khenthuong</returns>
+        List<ThongTinKhenThuong> GetAllKhenThuong();
+    }
+}
