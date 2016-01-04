7803b7897937aef60be8f19ede1a6f0eaa4dbe6a
Nguyen Hai Dang <nguyenhaidang.uit@gmail.com>
2016-01-04 22:11:16 +0700

    commit file bootstrap, jquery...

---
+++ SMS/SMS.DATA/Models/IntOption.cs
@@ -0,0 +1,8 @@
+﻿namespace SMS.DATA.Models
+{
+    public class IntOption
+    {
+        public int Value { get; set; }
+        public string DisplayText { get; set; }
+    }
+}
---
+++ SMS/SMS.DATA/Models/StringOption.cs
@@ -0,0 +1,12 @@
+﻿
+namespace SMS.DATA.Models
+{
+    /// <summary>
+    /// class use for bind data to jtable
+    /// </summary>
+    public class StringOption
+    {
+        public string Value { get; set; }
+        public string DisplayText { get; set; }
+    }
+}
